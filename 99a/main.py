eb7e831a1293464686439e4f058d7eea3067e290
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-14 21:21:29 +1100

    Add files via upload

---
+++ 99a/main.py
@@ -0,0 +1,29 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a, b = I().split('.')
+if a[-1] != '9':
+    if int(b[0]) < 5:
+        print(int(a))
+    else:
+        print(int(a) + 1)
+else:
+    print("GOTO Vasilisa.")
