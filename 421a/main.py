160f269908dbae2dd7a7db39cf112b6996d3a337
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-14 21:21:43 +1100

    Add files via upload

---
+++ 421a/main.py
@@ -0,0 +1,32 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+n, a, b = MII()
+L1 = LII()
+L2 = LII()
+
+ans = []
+for i in range(1, n+1):
+    if i in L1:
+        ans.append(1)
+    else:
+        ans.append(2)
+print(*ans)
