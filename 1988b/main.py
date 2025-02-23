9311167f522111ea3e7d6aee2006fd01716acb64
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-23 22:27:00 +1100

    Add files via upload

---
+++ 1988b/main.py
@@ -0,0 +1,31 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    b = II()
+    s = I()
+    L = [s[0]]
+    for i in s[1:]:
+        if i != L[-1] or i == '1':
+            L.append(i)
+    # print(L)
+    print("YES" if L.count('1') > L.count('0') else "NO")
