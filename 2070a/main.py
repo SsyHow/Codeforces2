cc57ac22ab8fe9cd2eb4f2a9de866587870b6289
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-15 22:13:07 +1100

    Add files via upload

---
+++ 2070a/main.py
@@ -0,0 +1,32 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    b = II()
+    x = b // 15
+    k = b % 15
+    if k >= 2:
+        print(x * 3 + 3)
+    elif k == 1:
+        print(x * 3 + 2)
+    elif k == 0:
+        print(x * 3 + 1)
