d6aa65cc1fa3f29a198582bad04408fb283e01b4
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-19 20:09:59 +1100

    Add files via upload

---
+++ 918b/main.py
@@ -0,0 +1,31 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a, b = MII()
+dic = {}
+for _ in range(a):
+    s = I()
+    x, y = s.split()
+
+    dic[y+';'] = x
+for _ in range(b):
+    k = I()
+    m, n = k.split()
+    print(k, '#' + dic[n])
