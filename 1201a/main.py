a035d8cd8ccea8fb06251b3e44f36a990000694e
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-22 19:41:09 +1100

    Add files via upload

---
+++ 1201a/main.py
@@ -0,0 +1,30 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from collections import Counter
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a, b = MII()
+L = []
+ans = 0
+for _ in range(a):
+    L.append(I())
+v = LII()
+L = list(zip(*L))
+for i in range(b):
+    ans += v[i] * (Counter(L[i]).most_common()[0][1])
+print(ans)
---
+++ 1225a/main.py
@@ -0,0 +1,30 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a, b = MII()
+
+if b - a == 1:
+    print(a, b)
+elif a == b:
+    print(a*10 + 1, b*10 + 2)
+elif a == 9 and b == 1:
+    print(9, 10)
+else:
+    print(-1)
