e6541cfcbccb9d93fa9f87093d076eb023413c4a
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-22 19:39:58 +1100

    Add files via upload

---
+++ 1668b/main.py
@@ -0,0 +1,27 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    n, m = MII()
+    L = sorted(MII())
+    # print(L, sum(L[1:]) , L[-1])
+    print("YES" if sum(L[1:]) + L[-1] + n <= m else "NO")
