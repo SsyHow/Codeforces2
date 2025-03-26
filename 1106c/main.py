102181fcd465085ca35132559de1b6b108ac5119
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-26 14:40:21 +1100

    Add files via upload

---
+++ 1106c/main.py
@@ -0,0 +1,30 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = sorted(MII())
+ans = 0
+p0, p1 = 0, a-1
+while p0 < p1:
+    ans += (L[p0] + L[p1]) ** 2
+    p0 += 1
+    p1 -= 1
+
+print(ans)
