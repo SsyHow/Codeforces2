b07bf07f3a617cf7949cec92317b6d34f837d042
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-26 14:40:10 +1100

    Add files via upload

---
+++ 1505a/main.py
@@ -0,0 +1,30 @@
+import sys
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+while True:
+    try:
+        x = input()
+        print("NO", flush=True)
+    except Exception as e:
+        break
+
+
---
+++ 1505b/main.py
@@ -0,0 +1,29 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+
+while a >= 10:
+    tmp = 0
+    while a > 0:
+        tmp += a % 10
+        a //= 10
+    a = tmp
+print(a)
