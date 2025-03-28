24a75512ada6ec7f861a8f59d355e088fcf64426
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-28 23:51:21 +1100

    Add files via upload

---
+++ 841a/main.py
@@ -0,0 +1,32 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from collections import defaultdict
+a, b = MII()
+s = I()
+dic = defaultdict(int)
+for i in s:
+    dic[i] += 1
+
+    if dic[i] > b:
+        print("NO")
+        break
+else:
+    print("YES")
