025d8ab5272d07a4a886ad1b13ceda1cf2be1649
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-29 23:27:12 +1100

    Add files via upload

---
+++ 1132b/main.py
@@ -0,0 +1,30 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+L = LII()
+L.sort()
+b = II()
+L2 = LII()
+
+s = sum(L)
+for i in L2:
+    print(s - L[-i])
