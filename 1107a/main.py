5eb1034d93475887a993220222f1bfaa890a9b72
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-03-30 23:34:23 +1100

    Add files via upload

---
+++ 1107a/main.py
@@ -0,0 +1,31 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    b = II()
+    s = I()
+    if b == 2 and s[:1] >= s[1:]:
+        print("NO")
+    else:
+        print("YES")
+        print(2)
+        print(s[:1], s[1:])
---
+++ 764b/main.py
@@ -0,0 +1,29 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+p0, p1 = 0, a - 1
+
+while p0 < p1:
+    L[p0], L[p1] = L[p1], L[p0]
+    p0 += 2
+    p1 -= 2
+print(*L)
