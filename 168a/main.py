77bac757a1b69e613ccd2e9b317b3baf81dc9588
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-03 23:25:11 +1100

    Add files via upload

---
+++ 168a/main.py
@@ -0,0 +1,25 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+n, x, y = MII()
+
+k = (y * n + 99) // 100
+
+print(max(0, k - x))
