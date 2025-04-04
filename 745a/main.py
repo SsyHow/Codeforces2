64f9a40d881334867cf7ba12dfacdccecc7bb7a8
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-04 17:48:56 +1100

    Add files via upload

---
+++ 745a/main.py
@@ -0,0 +1,29 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+s = I()
+
+k = set()
+
+for i in range(len(s)):
+    k.add(s[i:] + s[:i])
+# print(k)
+print(len(k))
