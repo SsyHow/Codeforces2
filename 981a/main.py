a29dda60d88fff5d96cb1dac38ebe08a2097c05a
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-06 23:50:39 +1000

    Add files via upload

---
+++ 981a/main.py
@@ -0,0 +1,28 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+s = I()
+
+if all(s[0] == s[i] for i in range(len(s))):
+    print(0)
+elif s == s[::-1]:
+    print(len(s) -1)
+else:
+    print(len(s))
