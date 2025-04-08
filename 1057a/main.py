ada5fd6589b688c218d79160d8d7b829b3534dd1
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-08 18:54:52 +1000

    Add files via upload

---
+++ 1057a/main.py
@@ -0,0 +1,28 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+L = [0] + LII()
+ans = [a]
+while a != 0:
+    a = L[a-1]
+    ans.append(a)
+print(*ans[:-1][::-1])
