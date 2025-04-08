5b7a810a60cf022180b198ba0a3dd79f3a2128a5
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-08 18:54:32 +1000

    Add files via upload

---
+++ 45a/main.py
@@ -0,0 +1,25 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+dic = {"January" : 0, "February" : 1, "March":2, "April":3, "May":4, "June":5, "July":6, "August":7, "September":8, "October":9, "November":10, "December":11}
+dicrev = {v :k for k, v in dic.items()}
+
+print(dicrev[(dic[I()] + II())%12])
---
+++ 793a/main.py
@@ -0,0 +1,32 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+n, k = MII()
+L = LII()
+
+ll = min(L)
+ans = 0
+for i in L:
+    if (i - ll) % k != 0:
+        print(-1)
+        break
+    ans += (i - ll) // k
+else:
+    print(ans)
