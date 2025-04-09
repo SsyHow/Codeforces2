daf0004ce535517aa9b4d8383ab505f79885f82f
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-09 22:50:41 +1000

    Add files via upload

---
+++ 1521a/main.py
@@ -0,0 +1,31 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    x, y = MII()
+    if y == 1:
+        print("NO")
+        continue
+    if y == 2:
+        y *= 2
+    print("YES")
+    print(x, x * (y - 1), x * y)
---
+++ 450a/main.py
@@ -0,0 +1,28 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a, b = MII()
+L = LII()
+M = [(i + b - 1)//b for i in L]
+x = max(M)
+for i in range(a-1, -1, -1):
+    if M[i] == x:
+        print(i + 1)
+        break
