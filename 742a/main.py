e53022b35777554a12e28873420b8f94e815b74d
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-09 22:49:58 +1000

    Add files via upload

---
+++ 742a/main.py
@@ -0,0 +1,27 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = [8, 4, 2, 6]
+
+if a == 0:
+    print(1)
+else:
+    print(L[a % 4 - 1])
