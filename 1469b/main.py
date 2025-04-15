fe0e32a95f41437e3c8d4243e3c08b530f198842
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-15 12:40:03 +1000

    Add files via upload

---
+++ 1469b/main.py
@@ -0,0 +1,29 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+from itertools import accumulate
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+t = II()
+for _ in range(t):
+    r = II()
+    L1 = accumulate([0] + LII())
+    b = II()
+    L2 = accumulate([0] + LII())
+
+    print(max(L1) + max(L2))
