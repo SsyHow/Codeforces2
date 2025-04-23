6df41adbeb5659c7e18febc2563d5d54a2e3adc2
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-23 22:39:27 +1000

    Add files via upload

---
+++ 2094a/main.py
@@ -0,0 +1,26 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    s1, s2, s3 = I().split()
+
+    print(s1[0] + s2[0] + s3[0])
---
+++ 2094b/main.py
@@ -0,0 +1,33 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+t = II()
+for _ in range(t):
+    n, m, l, r = MII()
+    x = y = 0
+    if m <= r:
+        y = m
+    elif m > r:
+        y = r
+        m -= r
+        x = -m
+
+    print(x, y)
+
