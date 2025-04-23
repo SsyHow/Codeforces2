9377142be9208e57105a23eb916a79df0a97d0e4
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-04-23 22:40:08 +1000

    Add files via upload

---
+++ 1607c/main.py
@@ -0,0 +1,35 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    b = II()
+    L = LII()
+    L.sort()
+    if b == 1:
+        print(L[0])
+        continue
+
+    ans = L[0]
+
+    for i in range(b - 1):
+        ans = max(ans, L[i + 1] - L[i])
+    print(ans)
