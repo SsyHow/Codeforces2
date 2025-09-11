9805b5dad7d63b2f861eaa3a2fb939077c342481
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-09-11 17:11:31 +1000

    Add files via upload

---
+++ 385a/main.py
@@ -0,0 +1,28 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+n, c = MII()
+L = LII()
+ans = 0
+for i in range(n - 1):
+    if L[i + 1] < L[i]:
+
+        ans = max(ans, L[i] - L[i + 1] - c)
+print(ans)
