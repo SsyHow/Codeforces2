0e94eaec31b5421f7ed5692d21e2b0013ef7e4f5
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-09-29 16:09:19 +1000

    Add files via upload

---
+++ 137b/main.py
@@ -0,0 +1,34 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+from collections import Counter
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+L = LII()
+
+ans = 0
+c = Counter(L)
+for i in c:
+    if i <= a:
+        ans += c[i] - 1
+    else:
+        ans += c[i]
+print(ans)
+
---
+++ 991a/main.py
@@ -0,0 +1,26 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a, b, c, n = MII()
+k = n - (a + b - c)
+if k < 1 or c > b or c > a or n < a or n < b or n < c:
+    print(-1)
+else:
+    print(k)
