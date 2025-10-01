d7a48af9ccf92e3b0dd2167171e04d78a4c86ba5
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-01 22:26:39 +1000

    Add files via upload

---
+++ 886b/main.py
@@ -0,0 +1,27 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from collections import defaultdict
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+dic = defaultdict(int)
+for i in range(a):
+    dic[L[i]] = i
+
+print(sorted(dic.items(), key = lambda x: x[1])[0][0])
