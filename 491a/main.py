be70c4ee73a74c8b2e0fbd1a1f0677d6b148c9ef
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-05 03:47:26 +1100

    Add files via upload

---
+++ 491a/main.py
@@ -0,0 +1,28 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+b = II()
+
+n = a + b + 1
+
+L = list(range(1, n + 1))
+
+print(*L[b:], *(L[:b][::-1]))
---
+++ 649a/main.py
@@ -0,0 +1,34 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+def check(x):
+    return (x & -x).bit_length() - 1
+
+from collections import defaultdict
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+ans = 0
+d = defaultdict(int)
+for i in L:
+    k = check(i)
+    d[k] += 1
+    ans = max(ans, k)
+print(2 ** ans, d[ans])
+
