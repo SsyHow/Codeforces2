b1e9deed26936f020ab8cdf68817a4d0b4ba1116
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-13 23:22:50 +1100

    Add files via upload

---
+++ 1791e/main.py
@@ -0,0 +1,34 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+    L = LII()
+
+    o = 0
+    mn = 0
+    for i in L:
+        if i < 0:
+            o += 1
+    if o & 1 == 1:
+        print(sum(map(abs, L)) - 2 *  min(map(abs, L)))
+    else:
+        print(sum(map(abs, L)))
