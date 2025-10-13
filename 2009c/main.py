6d3240d24e823b7110dd9c79954d243e723d5c64
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-13 23:23:36 +1100

    Add files via upload

---
+++ 2009c/main.py
@@ -0,0 +1,30 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    x, y, k = MII()
+    t = (x + k - 1) // k * 2
+    l = (y + k - 1) // k * 2
+    if t > l:
+        print(t -1)
+    else:
+        print(max(t, l) )
+
---
+++ 227b/main.py
@@ -0,0 +1,33 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+b = II()
+M = LII()
+
+c = dict(zip(L, range(1, a + 1)))
+
+x, y = 0, 0
+for i in M:
+    x += c[i]
+    y += a + 1 - c[i]
+
+print(x, y)
