73a7f242e979f26523c54753b1d2f22dd6c31dad
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-17 23:47:11 +1100

    Add files via upload

---
+++ 1800c2/main.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+import heapq
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+    L = LII()
+
+    k = []
+    ans = 0
+    for i in L:
+        if i == 0:
+            if not k:
+                continue
+            else:
+                ans += -heapq.heappop(k)
+        else:
+            heapq.heappush(k, -i)
+    print(ans)
