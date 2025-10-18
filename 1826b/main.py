f5a901ec2f878c3cc620cac364c29ec864860013
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-18 18:48:45 +1100

    Add files via upload

---
+++ 1826b/main.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from math import gcd
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+    L = LII()
+
+    i = 0
+    ans = 0
+    while i < b // 2:
+        ans = gcd(ans, abs(L[i] - L[b - i - 1]))
+        i += 1
+    print(ans)
---
+++ 2125b/main.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from math import gcd
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+t = II()
+for _ in range(t):
+    a, b, k = MII()
+    m = gcd(a, b)
+    x, y = a//m, b//m
+
+    if x <= k and y <= k:
+        print(1)
+    else:
+        print(2)
