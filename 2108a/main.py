b2d3edefb999f210cbbaca20d9c011870606bf07
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-18 18:46:13 +1100

    Add files via upload

---
+++ 2108a/main.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+from itertools import permutations
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for i in range(a):
+    b = II()
+    print(b ** 2 // 4 + 1)
---
+++ 2111a/main.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+
+    k = b // 2
+    ans = 3
+    while k :
+        ans += 2
+        k //= 2
+    print(ans)
