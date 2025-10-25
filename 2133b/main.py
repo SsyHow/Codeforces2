bc26a99e015dcf383b19f42e2f52cc6ec6855973
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-25 19:24:35 +1100

    Add files via upload

---
+++ 2133b/main.py
@@ -0,0 +1,33 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+    L = LII()
+
+    L.sort(reverse=True)
+
+    print(sum(L[i] for i in range(0, b, 2)))
