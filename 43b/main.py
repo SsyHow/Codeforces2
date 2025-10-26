65038ece6870a4ad78c2e671e3735b602a9c8c9a
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-26 21:49:52 +1100

    Add files via upload

---
+++ 43b/main.py
@@ -0,0 +1,37 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from collections import Counter
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+s = I()
+t = I()
+c = Counter(s)
+x = Counter(t)
+for i in x:
+    if i == ' ':
+        continue
+    if x[i] > c[i]:
+        print("NO")
+        break
+else:
+    print("YES")
