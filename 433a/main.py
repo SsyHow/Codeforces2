2018dff84efe119bc9fe6cd6ca86323332102e55
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-26 21:50:17 +1100

    Add files via upload

---
+++ 433a/main.py
@@ -0,0 +1,35 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LI()
+c = L.count('100')
+d = a - c
+if c & 1 == 0 and c > 0:
+    print("YES")
+elif d & 1 == 0 and c & 1 == 0:
+    print("YES")
+else:
+    print("NO")
