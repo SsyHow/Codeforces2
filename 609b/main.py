819c5f43adbecc12c99e8143ebe92501e3d43879
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-26 21:50:39 +1100

    Add files via upload

---
+++ 609b/main.py
@@ -0,0 +1,33 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from collections import Counter
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a, b = MII()
+L = LII()
+c = list(Counter(L).values())
+ans = 0
+for i in range(b):
+    for j in range(i + 1, b):
+        ans += c[i] * c[j]
+print(ans)
