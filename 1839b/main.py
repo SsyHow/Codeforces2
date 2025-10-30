ceb63144931555a2db7786833fc3f33cd3c8210c
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-30 13:46:39 +1100

    Add files via upload

---
+++ 1839b/main.py
@@ -0,0 +1,39 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from collections import defaultdict
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+    L = defaultdict(list)
+
+    for _ in range(b):
+        x, y = MII()
+        L[x].append(y)
+    ans = 0
+    for i in L:
+        L[i].sort(reverse=True)
+    for i in L:
+        ans += sum(L[i][:i])
+    print(ans)
---
+++ 719a/main.py
@@ -0,0 +1,38 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+
+if L[-1] == 15:
+    print("DOWN")
+elif L[-1] == 0:
+    print("UP")
+elif a == 1:
+    print(-1)
+elif L[-2] > L[-1]:
+    print("DOWN")
+else:
+    print("UP")
