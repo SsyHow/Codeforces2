e649812b60fc2f0706874371531e345de1ff6b62
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-10-30 13:48:02 +1100

    Add files via upload

---
+++ 1087b/main.py
@@ -0,0 +1,30 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+n, k = MII()
+x = k - 1
+while n % x:
+    x -= 1
+print(n // x * k +x)
