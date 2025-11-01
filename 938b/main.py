676f2b703f3b4e814dfbad3501c4c93e66b25321
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-01 23:12:47 +1100

    Add files via upload

---
+++ 938b/main.py
@@ -0,0 +1,33 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+L = LII()
+ans = 0
+for i in L:
+    x = 10 ** 6 - i
+    y = i - 1
+    ans = max(ans, min(x, y))
+print(ans)
