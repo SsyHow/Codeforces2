1f15898857c2fefacf9706957d0f0825956aa8ec
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-01 23:13:10 +1100

    Add files via upload

---
+++ 2149a/main.py
@@ -0,0 +1,38 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+t = II()
+for _ in range(t):
+    b = II()
+    l = LII()
+
+    neg = 0
+    ans = 0
+    for i in l:
+        if i < 0:
+            neg += 1
+        elif i == 0:
+            ans += 1
+    print(ans + (neg % 2) * 2)
---
+++ 2149b/main.py
@@ -0,0 +1,35 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+t = II()
+for _ in range(t):
+    b = II()
+    L = LII()
+
+    L.sort()
+    ans = 0
+    for i in range(0, b, 2):
+        ans = max(ans, L[i + 1] - L[i])
+    print(ans)
