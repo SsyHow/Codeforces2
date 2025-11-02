47a99a4b67c132a41141a02f0177e420b125319a
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-02 14:48:28 +1100

    Add files via upload

---
+++ 1366a/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+func solve(in *bufio.Reader, out *bufio.Writer) {
+	var b, c int
+	Fscan(in, &b, &c)
+	Fprintln(out, min(b, c, (b+c)/3))
+}
+
+func run(r io.Reader, w io.Writer) {
+	in := bufio.NewReader(r)
+	out := bufio.NewWriter(w)
+	defer out.Flush()
+	var a int
+	Fscan(in, &a)
+	for i := 0; i < a; i++ {
+		solve(in, out)
+	}
+
+}
+
+func main() {
+	run(os.Stdin, os.Stdout)
+}
