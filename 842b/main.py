d0a5e0b6279ab706f1de6c0207e01f862bdfd8ba
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-09 04:09:20 +1100

    Add files via upload

---
+++ 842b/main.py
@@ -0,0 +1,37 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+r, d = MII()
+
+n = II()
+ans = 0
+for _ in range(n):
+    x, y, ri = MII()
+
+    k = (x * x + y * y) ** 0.5
+    if k - ri >= r - d and k + ri <= r:
+        ans += 1
+
+print(ans)
